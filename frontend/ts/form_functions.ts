d5dbf9251da76f2cb502062e8fd0eb12fa228e92
Daniel Fajt <daniel29se@gmail.com>
2022-03-06 22:26:58 +0100

    search ares update 1

--- frontend/ts/form_functions.ts
+++ frontend/ts/form_functions.ts
@@ -24,3 +24,57 @@ $(function(){
 
    $('form.dirty-check').areYouSure({'message':"Ve formuláři jsou neuložené změny."});
 });
+
+
+
+
+
+$(function () {
+
+    // Search by company IN button
+    const searchIcoBtn = document.createElement("button");
+    searchIcoBtn.innerText = "Vyhledat";
+    searchIcoBtn.classList.add('btn', 'btn-primary');
+
+    // Toggle Btn
+    const toggleBtn = document.createElement("input")
+    toggleBtn.type = 'checkbox'
+    toggleBtn.classList.add('form-check-input')
+
+    // Modal wrapper NODE
+    let modalNode = document.getElementById('fm-modal').querySelector('.modal-wrapper');
+
+    // Create an observer instance.
+    var observer = new MutationObserver(function (mutations) {
+        console.log(modalNode);
+        let icoInputEl = modalNode.querySelector('#id_ico');
+        let companyDivEl = modalNode.querySelector('#div_id_name');
+        let buttonsEl = modalNode.querySelector('.btn-primary');
+        // console.log('BUT', buttonsEl)
+
+        // Hide company name field
+        companyDivEl.style.display = 'none';
+        // Hide original Django form submit button
+        // buttonsEl.style.display = 'none'
+
+        // Replace submit button with toggle button
+        buttonsEl.parentNode.replaceChild(searchIcoBtn, buttonsEl)
+
+        // Add toggle Btn
+        modalNode.querySelector('.modal-body').appendChild(toggleBtn)
+
+
+        icoInputEl.addEventListener('input', function (e) {
+            console.log(e.target.value)
+        })
+
+    });
+
+    // Pass in the target node, as well as the observer options.
+    observer.observe(modalNode, {
+        attributes: true,
+        childList: true,
+        characterData: true
+    })
+
+})
